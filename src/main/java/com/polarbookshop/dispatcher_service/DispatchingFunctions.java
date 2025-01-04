bc63e20f2a901691db354e51dd3459034a7d019e
nagarjuna-guntu <iamnagarjuna.guntu@gmail.com>
2025-01-04 07:50:34 +0530

    otel changes

---
+++ src/main/java/com/polarbookshop/dispatcher_service/DispatchingFunctions.java
@@ -0,0 +1,33 @@
+package com.polarbookshop.dispatcher_service;
+
+import java.util.function.Function;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
+
+@Configuration
+@Slf4j
+public class DispatchingFunctions {
+	 
+	 @Bean
+	 Function<OrderAcceptedMessage, Long> pack() {
+		return orderAcceptedMessage -> {
+			log.info("The order with id {} is packed.", orderAcceptedMessage.orderId());
+			return orderAcceptedMessage.orderId();
+		};
+		 
+	 }
+	 
+	 @Bean
+	 Function<Flux<Long>, Flux<OrderDispatchedMessage>> label() {
+		return orderFlux -> orderFlux.map(orderId -> {
+			log.info("The order with id {} is labeled.", orderId);
+			return new OrderDispatchedMessage(orderId);	
+		});
+		}
+	 
+
+}
---
+++ src/test/java/com/polarbookshop/dispatcher_service/DispatchingFunctionsIntegrationTests.java
@@ -0,0 +1,59 @@
+package com.polarbookshop.dispatcher_service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.function.Function;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.function.context.FunctionCatalog;
+import org.springframework.cloud.function.context.test.FunctionalSpringBootTest;
+
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+@FunctionalSpringBootTest
+@Disabled("These tests are only necessary when using the functions alone (no bindings)")
+public class DispatchingFunctionsIntegrationTests {
+	
+	@Autowired
+	private FunctionCatalog catalog;
+
+	@Test
+	void packOrder() {
+		Function<OrderAcceptedMessage, Long> pack = 
+				catalog.lookup(Function.class, "pack");
+		long orderId = 121;
+		var input = new OrderAcceptedMessage(orderId);
+		assertThat(pack.apply(input)).isEqualTo(orderId);
+	}
+
+	@Test
+	void labelOrder() {
+		Function<Flux<Long>, Flux<OrderDispatchedMessage>> label = 
+				catalog.lookup(Function.class, "label");
+		Flux<Long> orderId = Flux.just(121L);
+		var output = new OrderDispatchedMessage(121L);
+
+		StepVerifier.create(label.apply(orderId))
+				.expectNextMatches(dispatchedOrder ->
+						dispatchedOrder.equals(output))
+				.verifyComplete();
+	}
+
+	@Test
+	void packAndLabelOrder() {
+		Function<OrderAcceptedMessage, Flux<OrderDispatchedMessage>> packAndLabel =
+				catalog.lookup(Function.class, "pack|label");
+		long orderId = 121;
+		var input = new OrderAcceptedMessage(orderId);
+		var output = new OrderDispatchedMessage(orderId);
+
+		StepVerifier.create(packAndLabel.apply(input))
+				.expectNextMatches(dispatchedOrder ->
+						dispatchedOrder.equals(output))
+				.verifyComplete();
+	}
+
+}
